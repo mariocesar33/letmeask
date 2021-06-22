bf5a6bb50d23fe12becab22cdf03eee0009e3eb3
Mário César Silva <mcgato33@hotmail.com>
2021-06-22 18:34:14 -0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/newRoom";
 
 function App() {
 
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <NewRoom />
     </>
   );
 }
