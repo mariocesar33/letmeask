0bf7475b52e65be3b2e6d31bcf0a2872b6d50e40
Mário César Silva <mcgato33@hotmail.com>
2021-06-23 05:29:32 -0100

    navegação ao clicar num botão

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from "react-router-dom";
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
@@ -7,6 +9,12 @@ import { Button } from '../components/Button';
 import "../styles/auth.scss";
 
 export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push("/rooms/new")
+  }
+
   return(
     <div id="page-auth">
       <aside>
@@ -17,7 +25,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="letmeask logotipo" />
-          <button className="create-room">
+          <button className="create-room" onClick={navigateToNewRoom}>
             <img src={googleIconImg} alt="logo do google" />
             Craia sua sala com o google
           </button>
